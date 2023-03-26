d116b4a4ba448b7dc97a03824ea9a719872805d2
Kaffaborn <113623221+Kaffaborn@users.noreply.github.com>
2023-03-26 13:08:09 +0000

    Deploying to gh-pages from @ Kaffaborn/goit-js-hw-09@02491fff3b2e97a78a4c81f035318a7255337112 🚀

--- 03-promises.8ef0b199.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");function o(e,o){const t=Math.random()>.3;return new Promise(((n,i)=>{setTimeout((()=>{t?n({position:e,delay:o}):i({position:e,delay:o})}),o)}))}e.addEventListener("submit",(function(t){t.preventDefault();let n=Number(e.delay.value);const i=Number(e.step.value),l=e.amount.value;for(let e=1;e<=l;e++)o(e,n).then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)})),n+=i}));
-//# sourceMappingURL=03-promises.8ef0b199.js.map
